d698854fa1a1c554ab0e06b253435adba238f4ed
Reinhold B <reinhold.brant@gmx.de>
2020-06-19 13:53:02 +0200

    version 2

--- GUI.py
+++ GUI.py
@@ -6,20 +6,52 @@ import time
 import threading
 
 class SetupWindow:
-	SHEIGHT = 100
-	SWIDTH = 100
+	SHEIGHT = 250
+	SWIDTH = 500
+	BG_COLOR_SETUP = "#71d96a"
 
 	def start_setup(self):
+
+		#button hover functions
+		def enter(e):
+			start_btn["background"] = "#b3f0af"
+		def leave(e):
+			start_btn["background"] = "#ffffff"
+
+
 		master = Tk()
+		master.geometry(f"{SetupWindow.SWIDTH}x{SetupWindow.SHEIGHT}")
+		master.maxsize(700, 350)
+		master.title("Setup your Pomodoro Clock")
+		master.config(bg=SetupWindow.BG_COLOR_SETUP)
 
-		self.minute_entry = Spinbox(master, from_=0, to=500)
-		self.minute_entry.grid(row=0, column=0)
+		frame = Frame(master, bg=SetupWindow.BG_COLOR_SETUP)
+		frame.grid(row=0, column=0)
 
-		self.second_entry = Spinbox(master, from_=0, to=59)
-		self.second_entry.grid(row=0, column=1)
+		minute_label = Label(frame, text="minutes", font=("Roboto Slab", 10), bg="#71d96a",
+			fg="#ffffff")
+		minute_label.grid(row=0, column=0, sticky=S, pady=(20, 10))
+		self.minute_entry = Spinbox(frame, from_=0, to=500, font=("Roboto Extra Bold", 15))
+		self.minute_entry.grid(row=1, column=0, ipady=10)
 
-		start_btn = Button(master, text='Start Pomodoro', command=self.start_GUI)
-		start_btn.grid(row=1, column=0)
+		seconds_label = Label(frame, text="seconds", font=("Roboto Slab", 10), bg="#71d96a",
+			fg="#ffffff")
+		seconds_label.grid(row=2, column=0, sticky=S, pady=10)
+		self.second_entry = Spinbox(frame, from_=0, to=59, font=("Roboto Extra Bold", 15))
+		self.second_entry.grid(row=3, column=0, ipady=10)
+
+		start_btn = Button(frame, text='Start Pomodoro', command=self.start_GUI, font=("Roboto Slab Extra", 10)
+			,bg="#ffffff", borderwidth=0)
+		start_btn.grid(row=4, column=0, pady=30, ipady=10)
+		start_btn.bind("<Enter>", enter)
+		start_btn.bind("<Leave>", leave)
+
+		master.grid_rowconfigure(0, weight=1)
+		master.grid_columnconfigure(0, weight=1)
+
+		rows_setup = [0, 1, 2, 3, 4]
+		frame.grid_rowconfigure(rows_setup, weight=2)
+		frame.grid_columnconfigure(0, weight=2)
 
 		mainloop()
 
@@ -27,7 +59,7 @@ class SetupWindow:
 		minutes = int(self.minute_entry.get())
 		seconds = int(self.second_entry.get())
 
-		main_gui = GUI(300, 300, minutes, seconds)
+		main_gui = GUI(450, 700, minutes, seconds)
 		main_gui.build()
 
 
@@ -35,15 +67,9 @@ class GUI:
 
 	#base constants
 	WIN_NAME = "Pomodoro Timer"
-	BG_COLOR = "#fffff"
-	TEXT_COLOR = '#00000'
+	BG_COLOR = "#71d96a" #green: "#71d96a" blue: #1b3863
 
-	#text
-	TITLE_HEIGHT = 0
-	TITLE_WIDTH = 0
-
-	TIMER_HEIGHT = 0 
-	TIMER_WIDTH = 0
+	
 	def __init__(self, height, width, minutes, seconds):
 		self.win_height = height
 		self.win_width = width
@@ -52,7 +78,7 @@ class GUI:
 		self.time_in_sec = minutes * 60 + seconds
 
 		self.timer = Timer("Current")
-		self.timer.set_timer(self.minutes, self.seconds, "ALERT")
+		self.timer.set_timer(self.minutes, self.seconds, "It's time\nfor a break!")
 
 		self.stopThread = False
 		
@@ -64,43 +90,65 @@ class GUI:
 		#root
 		root = Tk()
 		root.title(GUI.WIN_NAME)
-		root.geometry(f'{self.win_height}x{self.win_width}')
+		root.geometry(f'{self.win_width}x{self.win_height}')
+		root.config(bg=SetupWindow.BG_COLOR_SETUP)
+
+		main_frame = Frame(root, bg=SetupWindow.BG_COLOR_SETUP)
+		main_frame.grid(row=1, column=0,)
 
 		#title text
-		clock_text = Label(root, height=GUI.TITLE_HEIGHT, width=GUI.TITLE_WIDTH, text=GUI.WIN_NAME)
-		clock_text.grid(row=0, column=0)
+		clock_text = Label(root, font=("Roboto Extra Bold", 30), text=GUI.WIN_NAME,
+			bg=SetupWindow.BG_COLOR_SETUP, fg="#ffffff")
+		clock_text.grid(row=0, column=0, pady=(60, 0))
 
 		#timer text
-		min_text_label = Label(root, height=GUI.TIMER_HEIGHT, width=GUI.TIMER_WIDTH, text=self.timer.minutes_to_go)
-		min_text_label.grid(row=1, column=1)
+		min_text_label = Label(main_frame, text=self.timer.minutes_to_go, 
+			font=("Roboto Slab", 90), bg=SetupWindow.BG_COLOR_SETUP, fg="#ffffff")
+		min_text_label.grid(row=0, column=0, pady=(40, 100), ipadx=30)
 
 		#timer text
-		sec_text_label = Label(root, height=GUI.TIMER_HEIGHT, width=GUI.TIMER_WIDTH, text=self.timer.seconds_to_go)
-		sec_text_label.grid(row=1, column=2)
+		sec_text_label = Label(main_frame, text=self.timer.seconds_to_go, 
+			font=("Roboto Slab", 90), bg=SetupWindow.BG_COLOR_SETUP, fg="#ffffff")
+		sec_text_label.grid(row=0, column=1, pady=(40, 100), ipadx=30)
 
-		self.refresh(root, sec_text_label, min_text_label)
+		self.refresh(main_frame, sec_text_label, min_text_label, root)
+
+		root.grid_rowconfigure(0, weight=1)
+		root.grid_columnconfigure(0, weight=1)
+
+		main_frame_cols = [0, 1]
+		main_frame.grid_rowconfigure(0, weight=2)
+		main_frame.grid_columnconfigure(main_frame_cols, weight=2)
 
 		root.mainloop()
 
-		#root.grid_rowconfigure(0, weight=1)
-		#root.grid_columnconfigure(0, weight=1)
-
-	def refresh(self, root, sec_text_label, min_text_label):
+	def refresh(self, root, sec_text_label, min_text_label, main_root):
 		self.timer.get_time_to_go()
 		if self.timer.time_to_go == 0 and self.timer.minutes_to_go == 0:
-			self.alert_and_stop(root)
+			self.alert_and_stop(main_root)
 		sec_text_label.configure(text=str(self.timer.time_to_go))
-		min_text_label.configure(text=str(self.timer.minutes_to_go))
-		t1 = threading.Timer(1, self.refresh, args=(root, sec_text_label, min_text_label))
+		min_text_label.configure(text=(str(self.timer.minutes_to_go), ":"))
+		t1 = threading.Timer(1, self.refresh, args=(root, sec_text_label, min_text_label, main_root))
 		if self.stopThread: t1.cancel()
 		t1.start()
 
-	def alert_and_stop(self, root):
-		root.withdraw()
+	def alert_and_stop(self, main_root):
+		main_root.withdraw()
 		messageBox = Tk()
-		messageBox.title("Time for a break :)")
-		alert = Label(messageBox, text=self.timer.alert_message,)
-		alert.config(font=('Courier', 40))
+		messageBox.title("Time for a break")
+		messageBox.geometry("500x250")
+		messageBox.config(bg=SetupWindow.BG_COLOR_SETUP)
+		alert = Label(messageBox, text=self.timer.alert_message, bg=SetupWindow.BG_COLOR_SETUP,
+			fg="#ffffff")
+		alert.config(font=('Roboto Slab', 25))
 		alert.grid(row=0, column=0)
+
+		messageBox.grid_rowconfigure(0, weight=1)
+		messageBox.grid_columnconfigure(0, weight=1)
+
+		messageBox.lift()
+		messageBox.attributes('-topmost',True)
+		messageBox.after_idle(messageBox.attributes,'-topmost',False)
+
 		messageBox.mainloop()
 		self.stopThread = True
